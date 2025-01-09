247c0d3a0adbd41562793d647cfc75f987da9e18
nana <150121149+adrianajfry04@users.noreply.github.com>
2025-01-09 14:41:07 +0800

    update Candidate class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream> 
 using namespace std; 
 
+struct nodeCandidate {
+    string name;
+    int age;
+    int slot;
+    string status;
+    nodeCandidate *next;
+};
+
 int main() 
 { 
     cout << "Hello, World!" << endl; 
